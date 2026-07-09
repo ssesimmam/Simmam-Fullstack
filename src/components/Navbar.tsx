032cf30aecd953563be56129e3ea191528d82d9c
sasvanthu <sasvanthu.g.2006@gmail.com>
2026-07-09 09:59:04 +0530

    Remove game-related files and disable game link

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,7 +11,7 @@ type NavLink = { href: string; label: string; disabled?: boolean };
 
 const leftLinks: NavLink[] = [
   { href: '/', label: 'Dashboard' },
-  { href: 'https://game.ssesimmam.com', label: 'Games' },
+  { href: 'https://game.ssesimmam.com', label: 'Games', disabled: true },
 ];
 
 const rightLinks: NavLink[] = [
