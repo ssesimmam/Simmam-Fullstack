e56885426a86d290f164548fb9a896acd959ed74
sasvanthu <sasvanthu.g.2006@gmail.com>
2026-07-12 09:53:43 +0530

    feat: enable game button

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,7 +11,7 @@ type NavLink = { href: string; label: string; disabled?: boolean };
 
 const leftLinks: NavLink[] = [
   { href: '/', label: 'Dashboard' },
-  { href: 'https://game.ssesimmam.com', label: 'Games', disabled: true },
+  { href: 'https://game.ssesimmam.com', label: 'Games' },
 ];
 
 const rightLinks: NavLink[] = [
